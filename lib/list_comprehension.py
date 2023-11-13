5e63d94a5d86bf7fbb388e1718e6accd6c98e5b4
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-11-12 19:43:41 -0500

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    new_list = [n for n in num_list if n % 2 == 0]
+    return new_list
+
     pass
 
 def make_exclamation(sentence_list):
+    new_list = [sentence + "!" for sentence in sentence_list]
+    return new_list
+
     pass
